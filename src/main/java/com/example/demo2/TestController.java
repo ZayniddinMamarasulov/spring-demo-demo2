dc8e18d0905802412404ba7adf4fd975761eb276
Zayniddin <dsasaMNs#d2>
2020-12-13 21:04:55 +0500

    google sign in added

--- src/main/java/com/example/demo2/TestController.java
+++ src/main/java/com/example/demo2/TestController.java
@@ -1,21 +1,33 @@
 package com.example.demo2;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
     @GetMapping
-    public ResponseEntity hello(){
-        return ResponseEntity.ok("Demo 2 Hello");
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("Hello, I AM DEMO APP");
+    }
+
+    @GetMapping("/restricted")
+    public String restricted() {
+        return "This text is only available for authenticated users";
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> userDetails(@AuthenticationPrincipal OAuth2User user) {
+        return user.getAttributes();
     }
 
     @PostMapping("message/{id}")
-    public ResponseEntity message(@PathVariable Long id){
+    public ResponseEntity message(@PathVariable Long id) {
         MessageDto dto = new MessageDto();
         dto.setId(id);
         dto.setMessage("Message from Demo2");
